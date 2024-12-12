8a9532d285a157451563896df8d46df09aba64f4
gaperez64 <gaperez64@gmail.com>
2024-12-12 10:35:16 +0100

    done done di done

--- day12.py
+++ day12.py
@@ -1,9 +1,10 @@
-import itertools
+from collections import defaultdict
 import copy
+import itertools
 import sys
 
 
-def silver(fname):
+def silver(fname, usesides=False):
     plan = []
     with open(fname, "r") as f:
         for line in f:
@@ -17,54 +18,64 @@ def silver(fname):
         visited[(x, y)] = True
         per = 0
         cnt = 1
-        hors = {}
-        vert = {}
+        hors = defaultdict(list)
+        vert = defaultdict(list)
         while len(tovisit) > 0:
             (i, j) = tovisit.pop()
-            for (a, b) in [(i + 1, j), (i - 1, j),
-                           (i, j + 1), (i, j - 1)]:
+            for (a, b) in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
                 if a < 0 or a >= len(plan):
                     per += 1
-                    if a + 1 in hors:
-                        hors[a + 1].add(j)
-                    else:
-                        hors[a + 1] = set([j])
+                    hors[a].append(j)
                 elif b < 0 or b >= len(plan[0]):
                     per += 1
-                    if b + 1 in vert:
-                        vert[b + 1].add(i)
-                    else:
-                        vert[b + 1] = set([i])
+                    vert[b].append(i)
                 elif plan[a][b] != plan[x][y]:
                     per += 1
                     if a != i:
-                        if a + 1 in hors:
-                            hors[a + 1].add(j)
-                        else:
-                            hors[a + 1] = set([j])
+                        hors[i + (a - i) / 4].append(j)
                     else:
-                        if b + 1 in vert:
-                            vert[b + 1].add(i)
-                        else:
-                            vert[b + 1] = set([i])
+                        vert[j + (b - j) / 4].append(i)
                 elif (a, b) not in visited:
                     tovisit.append(tuple([a, b]))
                     visited[(a, b)] = True
                     cnt += 1
-        print(f"Region of {plan[x][y]} has area {cnt} and perimenter {per}")
-        return cnt, per
+
+        # Now, per key in hors and vert, we need to sort and find gaps
+        sides = 0
+        print(hors)
+        for key in hors:
+            assert len(hors[key]) > 0
+            last = -2
+            for el in sorted(hors[key]):
+                if el - last > 1:
+                    sides += 1
+                last = el
+        print(vert)
+        for key in vert:
+            assert len(vert[key]) > 0
+            last = -2
+            for el in sorted(vert[key]):
+                if el - last > 1:
+                    sides += 1
+                last = el
+
+        print(f"Region of {plan[x][y]}: area {cnt}, per {per}, sides {sides}")
+        return cnt, per, sides
 
     total = 0
     for i in range(len(plan)):
         for j in range(len(plan[0])):
             if (i, j) not in visited:
-                area, perimeter = visit(i, j)
-                total += area * perimeter
+                area, perimeter, sides = visit(i, j)
+                if usesides:
+                    total += area * sides
+                else:
+                    total += area * perimeter
     return total
 
 
 def gold(fname):
-    pass
+    return silver(fname, usesides=True)
 
 
 if __name__ == "__main__":
