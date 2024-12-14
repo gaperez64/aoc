ae219289952804ccea1ceb8b837712522ae1a843
gaperez64 <gaperez64@gmail.com>
2024-12-14 17:19:22 +0100

    ridiculous exercise

--- day14.py
+++ day14.py
@@ -46,7 +46,47 @@ def silver(fname, shift=0):
 
 
 def gold(fname):
-    pass
+    machines = []
+    with open(fname, "r") as f:
+        for line in f:
+            line = line.strip()
+            p, v = line.split()
+            p = tuple(map(int, p[2:].split(",")))
+            v = tuple(map(int, v[2:].split(",")))
+            machines.append(tuple([p, v]))
+    # print(machines)
+
+    simtime = 1
+    dimx = 101
+    dimy = 103
+    secs = 0
+    while True:
+        rows = [[0] * dimx for _ in range(dimy)]
+        newmacs = []
+        for (p, v) in machines:
+            (x, y) = p
+            rows[y][x] += 1
+            (dx, dy) = v
+            x = (x + simtime * dx) % dimx
+            y = (y + simtime * dy) % dimy
+            newmacs.append(tuple([(x, y), v]))
+        machines = newmacs
+        disp = []
+        for y in range(dimy):
+            line = ""
+            for x in range(dimx):
+                if rows[y][x] == 0:
+                    line += '.'
+                else:
+                    line += str(rows[y][x])
+            disp.append(line)
+        if any(["111111111" in line for line in disp]):
+            print(f"{secs} seconds elapsed")
+            print("\n".join(disp))
+            _ = input()
+        secs += 1
+
+    return
 
 
 if __name__ == "__main__":
